559c12f9abb65a4e8400cf3932bb1b99e72683b5
dhsystembr <64677570+dhsystembr@users.noreply.github.com>
2020-05-26 07:29:58 -0300

    Update main.tf

--- 03-multi-providers/main.tf
+++ 03-multi-providers/main.tf
@@ -5,6 +5,43 @@ module "souzaec2-dev" {
   providers = {
     aws = aws.dev
   }
+
+  resource "aws_security_group" "allow+all" {
+  description = "Allow limited inbound external traffic"
+  name        = "terraform_ec2_private_sg"
+
+  ingress {
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+  }
+
+  ingress {
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8080
+    to_port     = 8080
+  }
+
+  ingress {
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+  }
+
+  egress {
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = {
+    Name = "ec2-private-sg"
+  }
+}
 }
 
 module "souzaec2-hom" {
