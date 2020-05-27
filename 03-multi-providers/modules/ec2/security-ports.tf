d2d0a6ca378332b1b8314cbe8b9f19fa51dd6535
dhsystembr <64677570+dhsystembr@users.noreply.github.com>
2020-05-27 08:35:23 -0300

    Update security-ports.tf

--- 03-multi-providers/modules/ec2/security-ports.tf
+++ 03-multi-providers/modules/ec2/security-ports.tf
@@ -1,3 +1,12 @@
+variable default_ingress-hom {
+  type = map(object({description = string, cidr_blocks = list(string)}))
+  default = {
+    22 = { description = "Inbound para SSH", cidr_blocks = [ "127.0.0.1/32" ] }
+    80 = { description = "Inbound para HTTP", cidr_blocks = [ "127.0.0.1/32" ] }
+    443 = { description = "Inbound para HTTPS", cidr_blocks = [ "127.0.0.1/32" ] }
+  }
+}
+
 variable default_ingress-dev {
   type = map(object({description = string, cidr_blocks = list(string)}))
   default = {
@@ -5,15 +14,6 @@ variable default_ingress-dev {
     80 = { description = "Inbound para HTTP", cidr_blocks = [ "127.0.0.1/32" ] }
     443 = { description = "Inbound para HTTPS", cidr_blocks = [ "127.0.0.1/32" ] }
     5432 = { description = "Inbound para postgres", cidr_blocks = [ "127.0.0.1/32" ] }
-  }
-}
-
-variable default_ingress-hom {
-  type = map(object({description = string, cidr_blocks = list(string)}))
-  default = {
-    22 = { description = "Inbound para SSH", cidr_blocks = [ "127.0.0.1/32" ] }
-    80 = { description = "Inbound para HTTP", cidr_blocks = [ "127.0.0.1/32" ] }
-    443 = { description = "Inbound para HTTPS", cidr_blocks = [ "127.0.0.1/32" ] }
   }
 }
 
