73e2ff123a7d627e6af7084768a41e205029980a
dhsystembr <64677570+dhsystembr@users.noreply.github.com>
2020-05-28 04:52:54 -0300

    asd

--- 03-multi-providers/modules/ec2/main.tf
+++ 03-multi-providers/modules/ec2/main.tf
@@ -16,18 +16,19 @@ variable "environment" {
   default = "dev"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "dev" {
   count = "${var.environment == "dev" ? 1 : 0}"
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.terraform_private_sgdev.name}"]
+  subnet_id = "${aws_subnet.public-subnet.id}"
   tags = {
     Name = "HelloWorld DEV"
     Env  = var.environment
   }
 }
 
-resource "aws_instance" "web2" {
+resource "aws_instance" "hom" {
   count = "${var.environment == "hom" ? 1 : 0}"
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
@@ -38,7 +39,7 @@ resource "aws_instance" "web2" {
   }
 }
 
-resource "aws_instance" "web3" {
+resource "aws_instance" "prod" {
   count = "${var.environment == "prod" ? 1 : 0}"
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
@@ -103,5 +104,35 @@ resource "aws_security_group" "terraform_private_sgprod" {
   }
   tags = {
     Name = "ec2-private-sgprod"
+  }
+}
+
+# Define our VPC
+resource "aws_vpc" "default" {
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+
+  tags {
+    Name = "test-vpc"
+  }
+}
+
+# Define the public subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.public_subnet_cidr}"
+
+  tags {
+    Name = "Web Public Subnet"
+  }
+}
+
+# Define the private subnet
+resource "aws_subnet" "private-subnet" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.private_subnet_cidr}"
+
+  tags {
+    Name = "Database Private Subnet"
   }
 }
--- 03-multi-providers/modules/ec2/security-ports.tf
+++ 03-multi-providers/modules/ec2/security-ports.tf
@@ -1,25 +1,25 @@
 variable default_ingress-hom {
   type = map(object({description = string, cidr_blocks = list(string)}))
   default = {
-    22 = { description = "Inbound para SSH", cidr_blocks = [ "10.0.0.0/16" ] }
-    80 = { description = "Inbound para HTTP", cidr_blocks = [ "10.0.0.0/16" ] }
-    443 = { description = "Inbound para HTTPS", cidr_blocks = [ "10.0.0.0/16" ] }
+    22 = { description = "Inbound para SSH", cidr_blocks = [ "127.0.0.1/32" ] }
+    80 = { description = "Inbound para HTTP", cidr_blocks = [ "127.0.0.1/32" ] }
+    443 = { description = "Inbound para HTTPS", cidr_blocks = [ "127.0.0.1/32" ] }
   }
 }
 
 variable default_ingress-dev {
   type = map(object({description = string, cidr_blocks = list(string)}))
   default = {
-    22 = { description = "Inbound para SSH", cidr_blocks = [ "10.0.1.0/16" ] }
-    80 = { description = "Inbound para HTTP", cidr_blocks = [ "10.0.1.0/16" ] }
-    443 = { description = "Inbound para HTTPS", cidr_blocks = [ "10.0.1.0/16" ] }
-    5432 = { description = "Inbound para postgres", cidr_blocks = [ "10.0.1.0/16" ] }
+    22 = { description = "Inbound para SSH", cidr_blocks = [ "127.0.0.1/32" ] }
+    80 = { description = "Inbound para HTTP", cidr_blocks = [ "127.0.0.1/32" ] }
+    443 = { description = "Inbound para HTTPS", cidr_blocks = [ "127.0.0.1/32" ] }
+    5432 = { description = "Inbound para postgres", cidr_blocks = [ "127.0.0.1/32" ] }
   }
 }
 
 variable default_ingress-prod {
   type = map(object({description = string, cidr_blocks = list(string)}))
   default = {
-    22 = { description = "Inbound para SSH", cidr_blocks = [ "10.0.2.0/16" ] }
+    22 = { description = "Inbound para SSH", cidr_blocks = [ "127.0.0.1/32" ] }
   }
 }
